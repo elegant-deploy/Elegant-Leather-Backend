b7810dce4c9d89394347b1a84f3d8302c325ecb8
elegant-deploy <elegantdeploy@gmail.com>
2026-02-07 15:47:00 +0500

    Update database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,5 +1,5 @@
 export const databaseConfig = {
-  uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/elegant-leather',
+  uri: process.env.MONGODB_URI || 'mongodb+srv://elegantdeploy_db_user:oRGs6GcekIm7HNiA@elegantcluster.4xoxhp8.mongodb.net/elegant-leather?retryWrites=true&w=majority',
 };
 
 console.log('Connecting to MongoDB:', databaseConfig.uri);
